c2c34fc24a1607e5d7c08822b8e86c0a4a2ff2b1
arslanahmad5978 <73576432+arslanahmad2512@users.noreply.github.com>
2022-10-01 19:22:18 +0500

    hero section background image

--- resources/views/frontend/partials/hero.blade.php
+++ resources/views/frontend/partials/hero.blade.php
@@ -1,6 +1,6 @@
 <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
-      <h1>Ali Haroon</h1>
+      <h1>{{$users->name}}</h1>
       <p>I'm <span class="typed" data-typed-items="{{$users->title}}"></span></p>
     </div>
 </section>
