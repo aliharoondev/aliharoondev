26c813432f0218257483017bf56f6e00ddced08e
Arslan Ahmad <arslanahmad5978@gmail.com>
2023-05-20 15:30:58 +0500

    changes

--- database/seeders/PortfolioDetailSeeder.php
+++ database/seeders/PortfolioDetailSeeder.php
@@ -18,23 +18,68 @@ class PortfolioDetailSeeder extends Seeder
         PortfolioDetail::create([
             'title'=>'App',
             'category'=>'App',
+            'client'=>'Us',
+            'portfolio_id'=>'1',
+            'project_date'=>'01-02-2020',
+            'project_url'=>'https://www.eccountant.co/',
             'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
-            'image'=>'portfolio/ios2.jpg',
-            'image2'=>'portfolio/ios2.jpg',
+            'image'=>'portfolio_detail/web.jpg',
+            'image2'=>'portfolio_detail/web.jpg',
         ]);
         PortfolioDetail::create([
             'title'=>'Ios',
             'category'=>'ios',
+            'client'=>'Us',
+            'portfolio_id'=>'2',
+            'project_date'=>'01-08-2020',
+            'project_url'=>'https://www.eccountant.co/',
             'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
-            'image'=>'portfolio/ios2.jpg',
-            'image2'=>'portfolio/ios2.jpg',
+            'image'=>'portfolio_detail/VandA.jpg',
+            'image2'=>'portfolio_detail/VandA.jpg',
         ]);
         PortfolioDetail::create([
             'title'=>'Web',
             'category'=>'Web',
+            'client'=>'Us',
+            'portfolio_id'=>'3',
+            'project_date'=>'01-02-2021',
+            'project_url'=>'https://www.eccountant.co/',
             'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
-            'image'=>'portfolio/ios2.jpg',
-            'image2'=>'portfolio/ios2.jpg',
+            'image'=>'portfolio_detail/triptax.jpg',
+            'image2'=>'portfolio_detail/triptax.jpg',
+        ]);
+        PortfolioDetail::create([
+            'title'=>'Web',
+            'category'=>'Web',
+            'client'=>'Us',
+            'portfolio_id'=>'4',
+            'project_date'=>'01-02-2021',
+            'project_url'=>'https://www.eccountant.co/',
+            'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
+            'image'=>'portfolio_detail/triptax.jpg',
+            'image2'=>'portfolio_detail/triptax.jpg',
+        ]);
+        PortfolioDetail::create([
+            'title'=>'Web',
+            'category'=>'Web',
+            'client'=>'Us',
+            'portfolio_id'=>'5',
+            'project_date'=>'01-02-2021',
+            'project_url'=>'https://www.eccountant.co/',
+            'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
+            'image'=>'portfolio_detail/triptax.jpg',
+            'image2'=>'portfolio_detail/triptax.jpg',
+        ]);
+        PortfolioDetail::create([
+            'title'=>'Web',
+            'category'=>'Web',
+            'client'=>'Us',
+            'portfolio_id'=>'6',
+            'project_date'=>'01-02-2021',
+            'project_url'=>'https://www.eccountant.co/',
+            'detail'=>'I am an aspiring freelance Ios Developer. I have more than 3 years of experience as a Ios I am a professional Ios Developer too. I am looking to get a better relationship with clients, I personally believe that client satisfaction is more importan',
+            'image'=>'portfolio_detail/triptax.jpg',
+            'image2'=>'portfolio_detail/triptax.jpg',
         ]);
     }
 }
